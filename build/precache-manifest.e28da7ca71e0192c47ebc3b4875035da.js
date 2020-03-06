c7b87f24c311bb180413356c6a22b2c24695549d
Ankit Sangwan <ankit.sangwan1999@gmail.com>
2020-03-06 19:46:50 +0530

    Homepage Changes

---
+++ build/precache-manifest.e28da7ca71e0192c47ebc3b4875035da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ff9611f397b82e83b3ed2cd0be343a2",
+    "url": "/Lottery-on-Ethereum/index.html"
+  },
+  {
+    "revision": "46af36af3720fa84fb67",
+    "url": "/Lottery-on-Ethereum/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a9db884ff3a6744be254",
+    "url": "/Lottery-on-Ethereum/static/js/2.25173d8f.chunk.js"
+  },
+  {
+    "revision": "bb834ae9b533cdce0a639dfdfdb72563",
+    "url": "/Lottery-on-Ethereum/static/js/2.25173d8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46af36af3720fa84fb67",
+    "url": "/Lottery-on-Ethereum/static/js/main.3be85fe0.chunk.js"
+  },
+  {
+    "revision": "ea6cf90eb1f2f3b999a5",
+    "url": "/Lottery-on-Ethereum/static/js/runtime-main.a908ee14.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 
-const appElement = <App/>;
+const appElement = <App name="Shaka Laka"/>;
 ReactDOM.render(appElement, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
